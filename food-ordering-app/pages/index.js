c5d0d5eecfc0610795073f6444ef70b5820236ad
nikola.milovanovic <nikolam998@gmail.com>
2022-06-23 21:18:32 +0200

    setup

---
+++ food-ordering-app/pages/index.js
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Gricko</title>
+        <meta name="description" content="Food ordering application" />
+        <link rel="icon" href="/favico.ico" />
+      </Head>
+      nesto
+    </div>
+  )
+}
